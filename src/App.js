0034fc83e9eb2dcb02b95bbd9f9e575e5c83d189
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-23 14:44:24 +0530

    Added Props to Card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-
+import logo1 from './images/k.png'
 function App() {
   return (
     <>
         <NavBar></NavBar>
         {/* <Hero></Hero> */}
-        <Card></Card>
+        <Card 
+          img = {logo1}
+          rating = "5.0"
+          reviewCount = {6}
+          country = "INDIA"
+          title = "Raees"
+          price = {136}
+        >
+
+        </Card>
     </>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import logo3 from '../images/star.png'
-import logo4 from '../images/k.png'
+// import logo4 from '../images/k.png'
 
-const Card = () => {
+const Card = (props) => {
+  // console.log(props)
   return (
     <div className='card'>
-        <img src={logo4} className='card--image' />
+        <img src={props.img} className='card--image' />
         <div className='card--stats'> 
             <img src={logo3} className="card--star"/>
-            <span>5.0</span>
-            <span className='gray'>(6) . </span>
-            <span className='gray'>USA</span>
+            <span>{props.rating}</span>
+            <span className='gray'>({props.reviewCount}) . </span>
+            <span className='gray'>{props.country}</span>
         </div>
-        <p>Life Lessons with Katie Zaferes</p>
-        <p><span className='bold'>From $136 </span> / person</p>
+        <p>{props.title}</p>
+        <p><span className='bold'>From ${props.price} </span> / person</p>
     </div>
   )
 }
