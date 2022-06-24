a6ad9a1947f391d1ea8f30fba60ac30681f16d93
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-24 17:12:15 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import logo1 from './images/k.png'
+import image1 from './images/2.jpg'
+import data from './data';
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card 
+      key = {item.id}
+      img = {image1}
+      // rating = {item.stats.rating}
+      // reviewCount = {item.stats.reviewCount}
+      // location = {item.location}
+      // title = {item.title}
+      // price = {item.price}
+      // openSpots = {item.openSpots}
+      //instead of passing like this we are passing as objects
+      item = {item}
+      // {...item} spread object as props internally like before
+      />
+
+    
+    )
+  })
+
   return (
     <>
         <NavBar></NavBar>
-        {/* <Hero></Hero> */}
-        <Card 
-          img = {logo1}
-          rating = "5.0"
-          reviewCount = {6}
-          country = "INDIA"
-          title = "Raees"
-          price = {136}
-        >
-
-        </Card>
+        <Hero></Hero>
+        <section className='cards-list'>
+          {cards}
+        </section>
+        
     </>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,17 +4,26 @@ import logo3 from '../images/star.png'
 
 const Card = (props) => {
   // console.log(props)
+  // console.log(props.openSpots)
+  let badgeText
+  if (props.openSpots === 0) {
+      badgeText = "SOLD OUT"
+  } else if (props.location === "Online") {
+      badgeText = "ONLINE"
+  }
   return (
     <div className='card'>
+        {/* {props.openSpots == 0 && <div className='card--badge'>SOLD OUT</div>} */}
+        {badgeText && <div className='card--badge'>{badgeText}</div>}
         <img src={props.img} className='card--image' />
         <div className='card--stats'> 
             <img src={logo3} className="card--star"/>
-            <span>{props.rating}</span>
-            <span className='gray'>({props.reviewCount}) . </span>
-            <span className='gray'>{props.country}</span>
+            <span>{props.item.stats.rating}</span>
+            <span className='gray'>({props.item.stats.reviewCount}) . </span>
+            <span className='gray'>{props.item.location}</span>
         </div>
-        <p>{props.title}</p>
-        <p><span className='bold'>From ${props.price} </span> / person</p>
+        <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
     </div>
   )
 }
